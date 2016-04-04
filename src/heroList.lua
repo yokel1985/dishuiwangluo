0ab036f32b698591806b4578f13d60c33d393906
yanghuan <1638840138@qq.com>
2016-04-04 22:08:43 +0800

    add code
    添加了英雄列表

--- src/heroList.lua
+++ src/heroList.lua
@@ -13,23 +13,39 @@ function HeroList.load()
 end
 function HeroList.show()
 	Util.addCurrentScene(HeroList.mHeroListLayer)
-    Util.layerToOther(HeroList.mHeroListLayer)
-
-	local function toBattleCallBack(ref,touchType)
-		local butName=ref:getName()
+    Util.layerToOther(HeroList.HeroList_layer)
+    local heroBtns = {}
+	local function showHeroCallBack(ref,touchType)
+		local btnTag=ref:getTag()
         if touchType==ccui.TouchEventType.began then
-	       
         	
         elseif touchType == ccui.TouchEventType.ended then 
-        	if  "Button_14" == butName then 
-	    		       		
-    		else
-	    		
-           end
-           -- effect
+
+        	for i=1,9 do
+        		if btnTag == i then
+					heroBtns[i]:loadTextures("UI/hero/43.png","UI/hero/44.png","")
+        		else
+					heroBtns[i]:loadTextures("UI/hero/44.png","UI/hero/44.png","")
+
+        		end
+        	end
         elseif touchType == ccui.TouchEventType.canceled then
 
         end
 	end
+
+	local root = HeroList.HeroList_layer:getChildByName("Panel_1")
+	local btnPanel = ccui.Helper:seekWidgetByName(root, "ScrollView_1")
+	for i=1,9 do
+		heroBtns[i] = ccui.Helper:seekWidgetByName(btnPanel, "Button_5_"..(i-1))
+		heroBtns[i]:setTag(i)
+		heroBtns[i]:loadTextures("UI/hero/44.png","UI/hero/44.png","")
+		if 1 == i then
+			heroBtns[i]:loadTextures("UI/hero/43.png","UI/hero/44.png","")
+			
+		end
+		heroBtns[i]:addTouchEventListener(showHeroCallBack)
+	end
+
 	Util.hide(HeroList.mHeroListLayer)
 end
